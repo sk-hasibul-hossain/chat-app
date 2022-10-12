24e345217a5eaf854e7d8f037aaffb21079fe1b9
Hasibul <skhasibulhossain16@gmail.com>
2022-10-12 18:52:59 +0530

    inisalized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+const config = {
+  apiKey: "AIzaSyBzF7HAkkssBpdd7X6JLOgn5nfmWOI5ZJ0",
+  authDomain: "chat-web-app-367d3.firebaseapp.com",
+  projectId: "chat-web-app-367d3",
+  storageBucket: "chat-web-app-367d3.appspot.com",
+  messagingSenderId: "17135478733",
+  appId: "1:17135478733:web:050714c0f1f1389755c684",
+};
+
+const app = firebase.initializeApp(config);
